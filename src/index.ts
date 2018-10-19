14091225a2b9c8a9ce9b0a448f8fbff2250283f5
Exulansis <rusu.eugeniu@gmx.de>
2018-10-19 12:00:25 +0200

    Fixed QR

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { JolocomLib, claimsMetadata } from 'jolocom-lib'
 import { InteractionType } from 'jolocom-lib/js/interactionFlows/types'
 
-const receiver = 'did:jolo:3cc34f1368bed3933b7b77b75b84815c718d5a219fdd77e9a1a03b513ec41925'
+const receiver = 'did:jolo:1db40fe8d6dc2cba10c269f6378717b9b233b72f60a68b6c6634c30ff32530a8'
 const privateIdentityKey = Buffer.from('7A59BC95029803EC082BFEBBA8094F9D6B58AAB1A23B7CA2D9C6B96F21E1A0A4', 'hex')
 const reg = JolocomLib.registry.jolocom.create()
 
